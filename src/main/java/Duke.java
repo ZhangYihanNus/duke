3fb8ac2a2fe8a76ba15586c6b3f79842df9c20b9
ZhangYihanNus <zhangyh0002@gmail.com>
2019-08-29 21:02:32 +0800

    A-Gradle_A-TextUiTesting_Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,33 @@
+import java.util.*;
+
 public class Duke {
+
+    static Scanner userInput = new Scanner(System.in);
+
+    public Duke() {
+        String startDuke = "____________________________________________________________\n" +
+                "   Hello! I'm Duke\n" +
+                "   What can I do for you?\n" +
+                "____________________________________________________________";
+
+        System.out.println(startDuke);
+
+         while(true) {
+             String cmd = userInput.nextLine();
+             if(cmd.equals("bye")) {
+                 String bye = "____________________________________________________________\n" +
+                         "\t"+"Bye! See you next time!\n" +
+                         "____________________________________________________________";
+                 System.out.println(bye);
+                 break;
+             }
+             String userCmd = "____________________________________________________________\n" +
+                    "\t"+cmd+"\n" +
+                    "____________________________________________________________";
+             System.out.println(userCmd);
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +35,8 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke theDuke = new Duke();
     }
+
 }
